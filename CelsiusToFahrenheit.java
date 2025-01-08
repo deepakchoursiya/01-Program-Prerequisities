64eae488db885ab8d711d0fe5ff997f4f7af247f
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-01-08 11:27:51 +0530

    Add program to covert celsius to fahreheit

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        // Converting Celsius to Fahrenheit
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + " Celsius is equal to " + fahrenheit + " Fahrenheit.");
+    }
+}
